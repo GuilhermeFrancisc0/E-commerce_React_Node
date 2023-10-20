c5521dc3d62e4a96ea72c54bbb15ea8d3b4b1905
Guilherme Francisco <guilhermefranciscoav@gmail.com>
2023-10-20 01:36:14 -0300

    (feature): #4 Criação do Controller Básico de Usuários

---
+++ backend/src/controllers/user/index.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from 'express';
+
+import { User } from '../../models/user';
+
+const data = {
+    users: [] as User[],
+    setUsers: function (data: User[]) { this.users = data }
+}
+
+export const getAll = (_: Request, res: Response) => {
+    res.json(data.users);
+}
+
+export const create = (req: Request, res: Response) => {
+    const { email, username, password }: User = req.body;
+
+    if (!email || !username || !password)
+        return res.status(400).json({ 'message': 'Campos "Email", "Usuário" e "Senha" são obrigatórios!' });
+
+    if (data.users.find(u => u.email === email))
+        return res.status(400).json({ 'message': 'Email já Cadastrado no Sistema!' });
+
+    if (data.users.find(u => u.username === username))
+        return res.status(400).json({ 'message': 'Usuário já Cadastrado no Sistema!' });
+
+    const newUser: User = {
+        id: data.users[data.users.length - 1]?.id + 1 || 1,
+        email,
+        username,
+        password,
+        permissions: ['CLIENT'],
+    };
+
+    data.setUsers([...data.users, newUser]);
+
+    res.json(newUser);
+}
+
+export const get = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const user = data.users.find(u => u.id === Number(id));
+
+    if (!user)
+        return res.status(400).send({ 'message': 'Nenhum usuário encontrado!' });
+
+    res.json(user);
+}
+
+export const remove = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const userIdx = data.users.findIndex(u => u.id === Number(id));
+
+    if (userIdx === -1)
+        return res.status(400).send({ 'message': 'Nenhum usuário encontrado!' });
+
+    data.users.splice(userIdx, 1);
+
+    res.json(data.users);
+}
+
+export const update = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const body: User = req.body;
+
+    const userIdx = data.users.findIndex(u => u.id === Number(id));
+
+    if (userIdx === -1)
+        return res.status(400).send({ 'message': 'Nenhum usuário encontrado!' });
+
+    if (!body.email || !body.username || !body.password)
+        return res.status(400).json({ 'message': 'Campos "Email", "Usuário" e "Senha" são obrigatórios!' });
+
+    if (data.users.find(u => u.email === body.email))
+        return res.status(400).json({ 'message': 'Email já Cadastrado no Sistema!' });
+
+    if (data.users.find(u => u.username === body.username))
+        return res.status(400).json({ 'message': 'Usuário já Cadastrado no Sistema!' });
+
+    data.setUsers(
+        data.users.map(u => {
+            if (u.id === Number(id))
+                return { ...u, ...body };
+            return u;
+        })
+    );
+
+    res.json(data.users);
+}
--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
+import { create, getAll, get, remove, update } from '../../controllers/user';
+
 const Routes = express.Router();
 
-Routes.get("/user", (req, res) => { });
-Routes.post("/user", (req, res) => { });
-Routes.get("/user/:id", (req, res) => { });
-Routes.put("/user/:id", (req, res) => { });
-Routes.delete("/user/:id", (req, res) => { });
+Routes.get("/user", getAll);
+Routes.post("/user", create);
+Routes.get("/user/:id", get);
+Routes.put("/user/:id", update);
+Routes.delete("/user/:id", remove);
 
 Routes.post("/auth", (req, res) => { });
 
